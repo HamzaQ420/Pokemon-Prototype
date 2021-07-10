c497953c36a8e615d9e4ae437de31892e0f4cfd4
HamzaQ420 <75277693+HamzaQ420@users.noreply.github.com>
2021-07-10 02:17:50 -0400

    Version 02
    -Added a rough save file system
    -Added stats
    -Added save file, as well as save file loading

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import time
+import time, glob
+from os import listdir
+from os.path import isfile, join
 
 # Goes at the beginning of the code
 print("")
 
-textDelayTime = .10
+textDelayTime = .40
 helpCommand = "//help"
 
 def start():
@@ -11,7 +13,31 @@ def start():
     startConfirm = startFile.read()
     startFile.close()
 
+    fileDirectory = "C:/Users/hamza/Documents/GitHub/Pokemon-Prototype/Files"
+
+    filesInDir = [f for f in listdir(fileDirectory) if isfile(join(fileDirectory, f))]
+
+    if "saveDat.txt" in filesInDir:
+        saveDatTrue = True
+    else:
+        saveDatTrue = False
+
     def PROTOCOL_START():
+        enterName = input("Please enter a name: ")
+
+        openSaveFile = open("Files/saveDat.txt", "r")
+        Lines = openSaveFile.readlines()
+        print(Lines)
+        openSaveFile.close()
+
+        Lines[0] = ""
+        Lines[0] = enterName
+
+        openSaveFile = open("Files/saveDat.txt", "w")
+        openSaveFile.writelines(Lines)
+
+        print("\nSave file created!")
+
         print("Welcome to this Python Pokemon Prototype!")
         time.sleep(textDelayTime)
         print("Here you will be able to catch Pokemon as well as battle with others!")
@@ -22,10 +48,18 @@ def start():
         startFile.truncate(0)
         startFile.write("yes")
     
+    if startConfirm == "yes" and saveDatTrue:
+        print("Loading previous save file!")
+
+        openSavDatName = open("Files/saveDat.txt", "r")
+        savDat = openSavDatName.readlines()
+        name = savDat[0]
+        name = name.replace("Trainer Name: ", "")
+        print("Hello Trainer", name)
+        openSavDatName.close()
+
     if startConfirm == "no":
         PROTOCOL_START()
-    else:
-        print("Loading previous save file!")
 
 start()
 
